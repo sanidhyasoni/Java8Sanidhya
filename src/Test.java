533428efe2b979b28d23581d7ac615c71d9aafbf
sanidhyasoni <sanidhyasoniit@gmail.com>
2024-05-13 14:42:28 -0500

    updated files

--- src/Test.java
+++
@@ -1,9 +0,0 @@
-
-public class Test {
-
-	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println("Adding second line to check git connection");
-	}
-
-}
---
+++ src/javasessions/Printing.java
@@ -0,0 +1,14 @@
+package javasessions;
+
+public class Printing {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println(100);
+		System.out.println(102.5);
+		System.out.println('A');
+		System.out.println('a');
+
+	}
+
+}
